64c572b68905533b2b70410aa1837088fd27df41
Karan Rahani <karanrahani6@gmail.com>
2026-07-05 10:44:01 +0530

    feat: add user routes and controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,2 +1,18 @@
 const userModel = require("../models/user.model");
 
+async function createUser(req, res) {
+  let createdUser = await userModel.create({
+    username: "James",
+
+    fullName: "JamesBond007",
+
+    email: "Bond@74.com",
+
+    password: "007",
+    posts: [],
+  });
+
+  res.status(201).json(createdUser);
+}
+
+module.exports = { createUser };
